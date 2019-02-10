7b097a0778dc92488bb0bd9f90ef0e53e7452526
simon.shimizu <simon80696@gmail.com>
2019-02-10 08:37:27 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
     root to: "tasks#index"
-  resources :tasks
+    get 'signup', to: 'users#new'
+  resources :users, only: [:index, :show, :new, :create]
 end
